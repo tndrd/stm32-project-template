404a3f0e91fdd6b90f436d63837c1f2538d82ba2
Vladimir Lekhterev <vovalekhterev@gmail.com>
2025-10-14 15:26:12 +0300

    Propagate more target definitions

--- devices/stm32f407vg/options.cmake
+++ devices/stm32f407vg/options.cmake
@@ -1,6 +1,10 @@
-set(DEVICE_FAMILY         STM32F407xx)
-set(DEVICE_CORE           cortex-m4  )
-set(DEVICE_FPU            fpv4-sp-d16)
+set(DEVICE_FAMILY_DEFS 
+  STM32F4
+  STM32F407xx
+)
+
+set(DEVICE_CORE           cortex-m4)
+set(DEVICE_FPU            auto)
 
 set(DEVICE_CMSIS_INCLUDE  lib/cmsis_device_f4/Include)
 set(DEVICE_STARTUP_SRC    lib/cmsis_device_f4/Source/Templates/gcc/startup_stm32f407xx.s)
